ec26d49a8372c13921a5f8c09def492a40ce9e1a
Junyong Jang <aszx6992@naver.com>
2023-03-19 22:28:54 +0900

    first commit

---
+++ src/main/java/stg/onyou/batch/jobs/feed/FeedBatchConfig.java
@@ -0,0 +1,41 @@
+package stg.onyou.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import stg.onyou.batch.common.JobListener;
+import stg.onyou.batch.common.StepListener;
+import stg.onyou.batch.jobs.feed.FeedWeightService;
+import stg.onyou.batch.jobs.feed.FeedWeightTasklet;
+
+@RequiredArgsConstructor
+@Configuration
+public class BatchConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private FeedWeightTasklet feedWeightTasklet;
+    @Autowired
+    private FeedWeightService feedWeightService;
+
+    @Bean
+    public Job myJob() {
+        return jobBuilderFactory.get("myJob")
+                .listener(new JobListener())
+                .start(myStep())
+                .build();
+    }
+
+    @Bean
+    public Step myStep() {
+        return stepBuilderFactory.get("myStep")
+                .listener(new StepListener())
+                .tasklet(feedWeightTasklet)
+                .build();
+    }
+
+}
